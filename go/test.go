48329fe54390721a3d1a8001b7a8057428b9e266
Steve Engledow <steve@offend.me.uk>
2014-08-23 00:27:26 +0100

    Start go implementation

---
+++ go/test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "offend.me.uk/babel"
+)
+
+func main() {
+    input, _ := ioutil.ReadFile("example.babel")
+
+    output := babel.BabelParse(string(input))
+
+    for k, v := range(output) {
+        fmt.Printf("%s -> '%s'\n", k, v)
+    }
+}
